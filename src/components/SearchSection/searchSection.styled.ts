3bb793e67053aa3625fe1679bb1303cac7bd8379
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2026-04-01 13:33:12 -0300

    fix: corrige bug de renderização dos card do holida

--- src/components/SearchSection/searchSection.styled.ts
+++ src/components/SearchSection/searchSection.styled.ts
@@ -62,13 +62,13 @@ export const SformInSection = styled.form<SformInSectionProps>`
   z-index: 100;
   transition: all ease-in-out 0.1s;
   width: 90vw;
-`;
+  `;
 
 export const Stitle = styled.h1`
   ${font({ fontKey: 'primary', sizeKey: 'lg' })}
   color: ${({ theme }) => theme.colors.title.primary};
   font-weight: 100;
-`;
+  `;
 
 export const ScontainerInput = styled.div<SsectionProps>`
   ${flex({})};
@@ -78,11 +78,11 @@ export const ScontainerInput = styled.div<SsectionProps>`
   @media (max-width: 1200px) {
     width: 70%;
   }
-
+  
   @media (max-width: 1000px) {
     width: 100%;
   }
-
+  
   label {
     position: absolute;
     width: 1px;
@@ -94,7 +94,7 @@ export const ScontainerInput = styled.div<SsectionProps>`
     white-space: nowrap;
     border: 0;
   }
-
+  
   input {
     ${flex({})};
     ${padding({ spaceKey: 'md' })};
@@ -105,13 +105,13 @@ export const ScontainerInput = styled.div<SsectionProps>`
     height: 100%;
     color: ${({ theme }) => theme.colors.text};
     background-color: ${({ theme }) => theme.colors.background.base};
-
+    
     &:focus {
       outline: none;
       border: solid 1px ${({ theme }) => theme.colors.border.active};
     }
   }
-`;
+  `;
 
 export const SContainerHoliday = styled.div`
   ${flex({})}
@@ -167,6 +167,7 @@ export const SContainerInfoHoliday = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+
 
   h2 {
     ${font({fontKey:"secondary", sizeKey:"xsm"})}
