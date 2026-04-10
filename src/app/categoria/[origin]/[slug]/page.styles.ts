319c31a007a662f856c7a0e517bb258318374ffa
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2026-04-09 22:43:11 -0300

    style: responsividade da página categoria e dos cards de produtos

--- src/app/categoria/[origin]/[slug]/page.styles.ts
+++ src/app/categoria/[origin]/[slug]/page.styles.ts
@@ -1,43 +1,54 @@
 'use client';
 
-import { flex, font, gap, padding } from "@/styles/mixins";
-import Link from "next/link";
-import styled from "styled-components";
+import { flex, font, gap, padding } from '@/styles/mixins';
+import styled from 'styled-components';
 
 export const SWrapper = styled.main`
-    position: relative;
-    ${flex({direction:'column', justfy:'start'})}
-    ${padding({spaceKey:'xlg'})}
-    ${gap({spaceKey:'xlg'})}
+  position: relative;
+  ${flex({ direction: 'column', justfy: 'start' })}
+  ${padding({ spaceKey: 'xlg' })}
+    ${gap({ spaceKey: 'xlg' })}
     background-color: ${({ theme }) => theme.colors.background.base};
-    width: 100%;
-    overflow: hidden;
-    background-color: ${({ theme }) => theme.colors.background.high};
+  width: 100%;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.colors.background.high};
 `;
 
 export const SContainerTitle = styled.div`
-    ${flex({direction:'column'})}
-    color: ${({ theme }) => theme.colors.title.primary};
+  ${flex({ direction: 'column' })}
+  color: ${({ theme }) => theme.colors.title.primary};
 
-    h1 {
-        ${font({fontKey:'primary', sizeKey:'xlg'})}
-        font-weight: 100;
-    }
+  h1 {
+    ${font({ fontKey: 'primary', sizeKey: 'xlg' })}
+    font-weight: 100;
+    text-transform: capitalize;
     
-    p{
-        ${font({fontKey:'secondary', sizeKey:'md'})}
-        font-weight: 100;
-    }
+  }
+
+  p {
+    ${font({ fontKey: 'secondary', sizeKey: 'md' })}
+    font-weight: 100;
+  }
 `;
 
 export const SSection = styled.section`
-    display: grid;
-    grid-template: 400px / 1fr 1fr 1fr 1fr;
-    ${gap({spaceKey:'md'})}
-    width: 100%;
-
+  ${flex({ justfy: 'start', align: 'start' })}
+  ${gap({ spaceKey: 'md' })}
+  flex-wrap: wrap;
+  width: 100%;
 `;
 
 export const SContainerProduct = styled.div`
-    ${flex({})}
+  ${flex({})}
+  flex: 0 0 100%;
+  height: 400px;
+  width: 100%;
+
+  @media (min-width: 606px) {
+      flex: 0 0 calc(50% - 8px);
+    }
+    
+    @media (min-width: 900px) {
+      flex: 0 0 calc(33.33% - 10px);
+  }
 `;
--- src/components/Cards/CardProduct/card.styled.ts
+++ src/components/Cards/CardProduct/card.styled.ts
@@ -4,13 +4,11 @@ import { borderRadius, flex, font, gap, padding } from '@/styles/mixins';
 import Link from 'next/link';
 
 export const Scard = styled.article`
-  ${flex({direction:'column'})}
-  ${gap({spaceKey: 'md'})} 
-  ${padding({spaceKey: 'md'})}
-  ${borderRadius({radiusKey:'md'})}
+  ${flex({ direction: 'column' })}
+  ${gap({ spaceKey: 'md' })} 
+  ${padding({ spaceKey: 'md' })}
+  ${borderRadius({ radiusKey: 'md' })}
   width: 100%;
-  min-width: 400px;
-  max-width: 600px;
   height: 100%;
   overflow: hidden;
   backdrop-filter: blur(10px);
@@ -18,29 +16,28 @@ export const Scard = styled.article`
 `;
 
 export const ScontainerSlider = styled.section`
-  ${(flex({}))}
-  ${gap({spaceKey: 'sm'})} 
-  ${padding({spaceKey: 'sm'})}
+  ${flex({})}
+  ${gap({ spaceKey: 'sm' })} 
+  ${padding({ spaceKey: 'sm' })}
   height: 60%;
 `;
 
 export const ScontainerImg = styled(Link)<{ $expand: number }>`
   ${flex({})}
   flex: ${({ $expand }) => $expand};
-  ${borderRadius({radiusKey:'md'})}
+  ${borderRadius({ radiusKey: 'md' })}
   height: 200px;
   min-width: 100px;
   overflow: hidden;
   transition: all 0.2s ease-in-out;
   cursor: pointer;
-  
 
   img {
     object-fit: cover;
     object-position: center;
     width: 100%;
     height: 100%;
-    transition: all ease-in-out .3s;
+    transition: all ease-in-out 0.3s;
 
     &:hover {
       scale: 120%;
@@ -51,17 +48,16 @@ export const ScontainerImg = styled(Link)<{ $expand: number }>`
 export const Scontent = styled.div`
   position: relative;
   justify-content: space-between;
-  ${flex({direction:'column', justfy:'space-between', align:'start'})}
-  ${gap({spaceKey: 'sm'})}
-  ${padding({spaceKey: 'sm'})}
+  ${flex({ direction: 'column', justfy: 'space-between', align: 'start' })}
+  ${gap({ spaceKey: 'sm' })}
+  ${padding({ spaceKey: 'sm' })}
   width: 100%;
   height: 40%;
   overflow: hidden;
   z-index: 1;
-  
 
   h1 {
-    ${font({fontKey:'primary', sizeKey:'xsm'})}
+    ${font({ fontKey: 'primary', sizeKey: 'xsm' })}
     font-weight: 100;
     color: ${({ theme }) => theme.colors.pallet.primaryUp};
     white-space: nowrap;
@@ -69,7 +65,7 @@ export const Scontent = styled.div`
   }
 
   p {
-    ${font({fontKey:'secondary', sizeKey:'sm'})}
+    ${font({ fontKey: 'secondary', sizeKey: 'sm' })}
     color: ${({ theme }) => theme.colors.text};
     width: 100%;
   }
