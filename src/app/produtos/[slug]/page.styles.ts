67fe3b27a82ab57463604059486d40b2ae1ee2cd
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2026-04-13 18:10:33 -0300

    style: melhora responsividade da page Produtos

--- src/app/produtos/[slug]/page.styles.ts
+++ src/app/produtos/[slug]/page.styles.ts
@@ -9,6 +9,14 @@ export const SWrapper = styled.main`
   height: 90dvh;
   overflow-y: scroll;
   background-color: ${({ theme }) => theme.colors.background.base};
+
+  @media (min-width: 550px) {
+    padding: 0 50px;
+  }
+
+  @media (min-width: 1000px) {
+    padding: 0 50px;
+  }
 `;
 
 export const SSection = styled.section`
@@ -16,10 +24,9 @@ export const SSection = styled.section`
   justify-self: start;
   align-self: start;
   grid-template-columns: 1fr;
-  grid-template-rows: auto auto auto auto;
+  grid-template-rows: auto auto auto;
   grid-template-areas:
     'smainImg smainImg smainImg'
-    'sgallery sgallery sgallery'
     'scontent scontent scontent '
     'sinfo sinfo sinfo';
   ${gap({ spaceKey: 'md' })}
@@ -27,6 +34,14 @@ export const SSection = styled.section`
   height: fit-content;
   padding-top: 10px;
   z-index: 0;
+
+  @media (min-width: 550px) {
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-template-rows: auto auto;
+    grid-template-areas:
+      'smainImg smainImg sinfo'
+      'scontent scontent scontent ';
+  }
 `;
 
 export const SContent = styled.div`
@@ -37,7 +52,6 @@ export const SContent = styled.div`
   width: 100%;
   height: fit-content;
   backdrop-filter: blur(30px);
-  border: solid 1.5px ${({ theme }) => theme.colors.opacity.base};
   overflow: hidden;
   background-color: ${({ theme }) => theme.colors.background.high};
 `;
@@ -48,7 +62,7 @@ export const STextContent = styled.div`
   ${gap({ spaceKey: 'md' })}
   ${padding({ spaceKey: 'lg' })}
 
-  h1 {
+    h1 {
     ${font({ fontKey: 'primary', sizeKey: 'md' })}
     color: ${({ theme }) => theme.colors.title.reverse};
     font-weight: 100;
@@ -119,27 +133,25 @@ export const SBtnContent = styled.div`
 `;
 
 export const SInfo = styled.div`
-    grid-area: sinfo;
-    ${flex({ direction: 'column', align: 'start' })};
-    ${borderRadius({ radiusKey: 'md' })}
-    ${gap({ spaceKey: 'sm' })}
-    width: 100%;
-    height: fit-content;
-    border: solid 1px ${({ theme }) => theme.colors.opacity.primary};
-    background-color: ${({ theme }) => theme.colors.background.high};
-    backdrop-filter: blur(30px);
-    overflow: hidden;
-`;
+  grid-area: sinfo;
+  ${flex({ direction: 'column', align: 'start' })};
+  ${borderRadius({ radiusKey: 'md' })};
+  ${gap({ spaceKey: 'sm' })};
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.background.high};
+  backdrop-filter: blur(30px);
+  overflow: hidden;
+  `;
 
 export const SType = styled.div`
-    ${flex({ justfy: 'start', align: 'center' })}
-    ${padding({ spaceKey: 'md' })}
-    background-color: ${({ theme }) => theme.colors.background.medium};
-    width: 100%;
-    height: fit-content;
-    
-    
-    h2 {
+  ${flex({ justfy: 'start', align: 'center' })}
+  ${padding({ spaceKey: 'md' })}
+  background-color: ${({ theme }) => theme.colors.background.medium};
+  width: 100%;
+  height: fit-content;
+  
+  h2 {
       ${flex({})}
       ${font({ fontKey: 'secondary', sizeKey: 'sm' })}
       color: ${({ theme }) => theme.colors.title.reverse};
@@ -150,47 +162,52 @@ export const SType = styled.div`
     `;
 
 export const ScontainerTag = styled.div`
-    ${flex({ direction:'column', align: 'start', justfy: 'space-between' })}
-    ${gap({ spaceKey: 'md' })}
+  ${flex({ direction: 'column', align: 'start', justfy: 'space-between' })}
+  ${gap({ spaceKey: 'md' })}
     ${padding({ spaceKey: 'md' })}
     width: 100%;
-    height: 100%;
-    `;
+  height: 100%;
+`;
 
 export const Stag = styled.div`
-    ${flex({ direction: 'column', align: 'start' })}
-    ${gap({ spaceKey: 'sm' })}
+  ${flex({ direction: 'column', align: 'start' })}
+  ${gap({ spaceKey: 'sm' })}
     width: 100%;
-    
-    h2 {
-        ${flex({ justfy: 'start' })}
-        ${font({ fontKey: 'secondary', sizeKey: 'sm' })}
+
+  h2 {
+    ${flex({ justfy: 'start' })}
+    ${font({ fontKey: 'secondary', sizeKey: 'sm' })}
         color: ${({ theme }) => theme.colors.title.reverse};
-        font-weight: 600;
-    }
-    `;
+    font-weight: 600;
+  }
+`;
 
 export const SContainerContentTag = styled.div`
-    ${flex({})}
-    width: 100%;
-    `; 
+${flex({})}
+width: 100%;
+`;
 
 export const SContainerItens = styled.div`
-  ${flex({justfy:'start'})}
+  ${flex({ justfy: 'start' })}
   ${gap({ spaceKey: 'md' })}
   width: 100%;
   overflow-x: scroll;
   white-space: nowrap;
-  
+
+  @media (min-width: 550px) {
+    flex-direction: column;
+  }
+
   span {
-      ${flex({})}
-      padding: 5px 15px;
-      ${gap({ spaceKey: 'sm' })}
-      ${font({ fontKey: 'secondary', sizeKey: 'sm' })}
-      ${borderRadius({ radiusKey: 'sm' })}
-      height: fit-content;
-      background-color: ${({ theme }) => theme.colors.background.surface};
-      font-weight: 600;
+    ${flex({justfy:'start'})}
+    ${gap({ spaceKey: 'sm' })}
+    ${font({ fontKey: 'secondary', sizeKey: 'sm' })}
+    ${borderRadius({ radiusKey: 'sm' })}
+    height: fit-content;
+    width: 100%;
+    padding: 5px 15px;
+    background-color: ${({ theme }) => theme.colors.background.surface};
+    font-weight: 600;
     font-style: italic;
   }
 `;
