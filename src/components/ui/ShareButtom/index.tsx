62a4bab0a4c6635f86874c6755f5163e6c204025
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2026-04-15 17:22:28 -0300

    fix: corrige bug de ordem de execução da variavel de estado isProduct

--- src/components/ui/ShareButtom/index.tsx
+++ src/components/ui/ShareButtom/index.tsx
@@ -3,58 +3,62 @@
 import { Product } from "@/utils/interfaces";
 import { IoMdShare } from "react-icons/io";
 import { SWrapper } from "./shareButtom.styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type propsCustomLink = {
-  children: React.ReactNode
-  className?: string
-  id?: string[]
-  product?: Product
+    children: React.ReactNode
+    className?: string
+    id?: string[]
+    product?: Product
 }
 
 // para compartilhar produtos únicos, o componente recebe o product, captura o id e armazena no dataID
 // para compartilhar catgorias, coleções ou favoritos, o componente recebe um array de id, transforma em string e armazena no dataID
 
-export default function ShareButtom( {className, children, id, product} : propsCustomLink){
-
+export default function ShareButtom({ className, children, id, product }: propsCustomLink) {
+    //usado para decidor qual rota usar
     const [isProduct, setIsProduct] = useState<boolean>(false);
 
+    useEffect(() => {
+        if (product?.slug) setIsProduct(true);
+    },[]);
+
     const getIds = () => {
+        // array de ids
         if (id && id.length > 0) {
             return id;
         }
+        // array com id individual
         if (product?.slug) {
-            setIsProduct(true)
             return [product.slug];
         }
         return [];
     }
 
     const handleShare = async () => {
-        
+
         const ids = getIds();
         if (!ids.length) return;
-        // passa os ids dos produtos via query para montar a coleção
+
+        // monta rota colecao, categorias, ou produtos baseado state
         const url = !isProduct ? `${window.location.origin}/colecao?ids=${ids.join(",")}`
-        : `${window.location.origin}/produtos/${ids.join()}`;
+            : `https://catalogo.miriammomesso.com.br/produtos/${ids.join()}`;
 
 
         try {
-            // Web Share API (mobile principalmente)
             if (navigator.share) {
                 await navigator.share({
-                title: "Confira essa coleção",
-                url,
+                    title: "Produtos Miriam Momesso",
+                    url,
                 });
             } else {
-                // fallback: copia pro clipboard
                 await navigator.clipboard.writeText(url);
                 alert("Link copiado!");
             }
-            } catch (err) {
+        } catch (err) {
             console.error("Erro ao compartilhar:", err);
-            }
         }
+    }
 
 
     return (
