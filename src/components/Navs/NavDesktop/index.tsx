e5b1665e941fc48951ec6b0bd9f800ad34e566ea
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2026-06-01 22:47:13 -0300

    feat: altera o nome na HeaderDesktop de meu kit para Coleções

--- src/components/Navs/NavDesktop/index.tsx
+++ src/components/Navs/NavDesktop/index.tsx
@@ -42,7 +42,7 @@ export default function NavDesktop() {
 
         <SItemMenu>
           <SLink href={'/colecao'}>
-            Meu Kit
+            Coleções
           </SLink>
         </SItemMenu>
       </SList>
